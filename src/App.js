d47daf337789cfede5a3f65b2079842c6617d189
daniele-frade <63023863+danielefrade@users.noreply.github.com>
2021-07-07 18:57:41 -0400

    Displayed basci structure of notes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import { Component } from 'react'
 const baseURL = 'http://localhost:3003'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      notes: [
+        {title: 'First Title', date: '12/12/2021', body: 'This is just the body of the note'},
+        {title: 'Second Title', date: '12/12/2021', body: 'This is just the body of the note'},
+      ]
+    }
+  }
  
   render() {
     return (
@@ -10,6 +19,18 @@ class App extends Component {
         <header>
           <h1>My Notes</h1>
         </header>
+        <div>
+          { this.state.notes.map(note => {
+              return(
+                <div>
+                  <p>{note.title}</p> 
+                  <p>{note.date}</p>
+                  <p>{note.body}</p>
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
     )
   }
