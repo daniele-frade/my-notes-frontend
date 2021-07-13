c85e16e88f5b2b25630d8ed644910e969a4f6011
daniele-frade <63023863+danielefrade@users.noreply.github.com>
2021-07-13 17:19:34 -0400

    Updated App.js to connect to Heroku api

--- src/AppWithRouterAccess.js
+++ src/AppWithRouterAccess.js
@@ -13,12 +13,12 @@ import Sidebar from './components/Sidebar';
 import Breadcrumbs from './components/Breadcrumbs'
 import Login from './components/Login'
 
-const apiURL = 'http://localhost:3003/my-notes'
+const apiURL = 'https://secure-plains-54293.herokuapp.com/'
 
 const oktaAuth = new OktaAuth({
   issuer: 'https://dev-68864381.okta.com/oauth2/default',
   clientId: '0oa17cxnn4No03oAI5d7',
-  redirectUri: 'http://localhost:3000/callback'
+  redirectUri: 'https://secure-plains-54293.herokuapp.com/callback'
 });
 
 const AppWithRouterAccess = () => {
